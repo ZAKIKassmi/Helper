9605ae769cca1a1577af375b32af50c13d8262a8
El Kassmi Zakariae <elkassimi.zakariae012@gmail.com>
2024-08-29 20:12:15 +0100

    make cards responsive

--- src/components/customCard.tsx
+++ src/components/customCard.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import CustomButton from './customButton';
+import Link from 'next/link';
 
 type Props = {
   title: string;
   description: string;
-  isSignUp: boolean
+  isSignUp: boolean;
+  className:string;
 }
 
-export default function CustomCard({title,description,isSignUp}: Props) {
+export default function CustomCard({title,description,isSignUp, className}: Props) {
   return (
-    <div className='max-w-[360px] p-8 border border-n-70 flex flex-col gap-6 rounded-lg'>
+    <div className={`min-w-[300px] p-8 border border-n-70 flex flex-col gap-6 rounded-lg min-h-[230px]  ${className}`}>
       <div className='flex flex-col gap-2'>
         <h1 className='text-n-700 text-h6-d font-bold'>{title}</h1>
         <p className='text-p-n text-n-100'>
@@ -17,7 +19,9 @@ export default function CustomCard({title,description,isSignUp}: Props) {
         </p>
       </div>
       {
-        isSignUp && <CustomButton type='primary' value='Sign Up'/>
+        isSignUp && <Link href="/signup">
+            <CustomButton type='primary' value='Sign Up'/>
+        </Link> 
       }
     </div>
   )
--- src/components/home/how-we-work.tsx
+++ src/components/home/how-we-work.tsx
@@ -1,12 +1,23 @@
+import { cardItems } from "@/lib/constants";
 import CustomCard from "../customCard";
 
 
 export default function HowWeWork() {
   
   return (
-    <div className="lg:px-40 lg:py-20">
-      <h1 className='text-h1-d font-semibold'>How Helper Works?</h1>
-      <CustomCard title="1. Create an account" description="Sign up easily and join our community by filling out a simple registration form." isSignUp={true}/>
+    <div className="lg:px-32 lg:py-20 px-8 py-20">
+      <h1 className='text-h1-d font-semibold mb-10'>How Helper Works?</h1>
+
+      
+
+      <div className="flex flex-wrap gap-5">
+        {
+          cardItems.map((item)=>(
+            <CustomCard key={item.title} className="flex-1" title={item.title} description={item.description} isSignUp={item.isSignUp}/>
+          ))
+        }
+      </div>
+      
     </div>
   )
 }

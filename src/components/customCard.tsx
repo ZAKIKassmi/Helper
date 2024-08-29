c24d0902ec0feb740d6232cf3daaf3e2bc144971
El Kassmi Zakariae <elkassimi.zakariae012@gmail.com>
2024-08-30 00:20:21 +0100

    add animation to cards

--- src/components/customCard.tsx
+++ src/components/customCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+"use client";
+import { cn } from '@/lib/utils';
 import CustomButton from './customButton';
 import Link from 'next/link';
-
+import { useEffect, useRef, useState } from "react";
 type Props = {
   title: string;
   description: string;
@@ -10,8 +11,34 @@ type Props = {
 }
 
 export default function CustomCard({title,description,isSignUp, className}: Props) {
+  const ref = useRef(null);
+
+  const [isIntersecting, setIsIntersecting] = useState<boolean>(false);
+  const scrollRef = useRef(0);
+
+  useEffect(()=>{
+    //entries represent the list of items that matche the observe item.
+    const observer = new IntersectionObserver((entries)=>{
+      const entry = entries[0];
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      if(scrollTop > scrollRef.current && entry.isIntersecting){
+        setIsIntersecting(true);
+      }
+      else if(scrollTop < scrollRef.current && !entry.isIntersecting){
+        setIsIntersecting(false);
+      }
+      
+
+      scrollRef.current = scrollTop <= 0 ? 0 : scrollTop;
+      
+    });
+    observer.observe(ref.current!);
+  },[]);
+
   return (
-    <div className={`min-w-[300px] p-8 border border-n-70 flex flex-col gap-6 rounded-lg min-h-[230px]  ${className}`}>
+    <div ref={ref} className={cn(`min-w-[300px] p-8 border border-n-70 flex flex-col gap-6 rounded-lg min-h-[230px]  ${className}`,{
+      "animate-translate-y-fade-in-fast-then-float": isIntersecting,
+    })}>
       <div className='flex flex-col gap-2'>
         <h1 className='text-n-700 text-h6-d font-bold'>{title}</h1>
         <p className='text-p-n text-n-100'>
--- src/components/home/how-we-work.tsx
+++ src/components/home/how-we-work.tsx
@@ -1,12 +1,15 @@
 import { cardItems } from "@/lib/constants";
 import CustomCard from "../customCard";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function HowWeWork() {
+
+ 
   
   return (
     <div className="lg:px-32 lg:py-20 px-8 py-20">
-      <h1 className='text-h1-d font-semibold mb-10'>How Helper Works?</h1>
+      <h1 className='text-p-n csz:text-h5-m sm:text-h3-m md:text-h2-m lg:text-h1-d font-bold text-n-900 mb-10'>How Helper Works?</h1>
 
       
 
